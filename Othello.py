9c188dcb2e6c1e9b27e24dc617182fe19a2837d7
Elis Grahn <egge5173@gmail.com>
2020-12-07 21:22:49 +0100

    Refactoring

--- Othello.py
+++ Othello.py
@@ -6,11 +6,9 @@ width = 8
 height = 8
 
 size = 100
-tick = 0
+turn = -1
 
 colors = [(0,144,103),(255,255,255),(70,70,70),(0,0,0)]
-ry = random.randint(0,height-1)
-rx = random.randint(0,width-1)
 radi = int(size/2)
 player = True
 
@@ -20,20 +18,27 @@ for i in range(0,2):
     arr[int(height/2-i)][int(width/2-i)] = 1
     arr[int(height/2+(0-i))][int(width/2+(i-1))] = -1
 
+arr[2][3] = 2
+arr[3][2] = 2 
+arr[4][5] = 2
+arr[5][4] = 2
+
+
 p.init()
 window = p.display.set_mode((size*width,size*height))
 p.display.set_caption('Othello!') 
 watch = p.time.Clock()
 
 def robot():
-    global ry,rx,player
-    while(arr[ry][rx] < 2):
-        ry = random.randint(0,height-1)
-        rx = random.randint(0,width-1)
+    global y,x,player
+    while(arr[y][x] < 2):
+        y = random.randint(0,height-1)
+        x = random.randint(0,width-1)
 
-    arr[ry][rx] = 1
-    player = True   
-    flip(ry,rx,1)
+    arr[y][x] = 1  
+    turn = 1
+    update()
+    player = True
 
 def human():
     global y,x,player
@@ -42,53 +47,76 @@ def human():
     y = math.floor(y/size)
     x = math.floor(x/size)
             
-    if(arr[y][x] == -2):
+    if(arr[y][x] == 2):
         arr[y][x] = -1
+        turn = -1
+        update()
         player = False
-        flip(y,x,-1)
 
-def flip(y,x,value):
-    for b in range(-1,2):
-        for a in range(-1,2):
+def corner(j,i):
+    global nj,pj,ni,pi
+    nj,pj = -1,2
+    ni,pi = -1,2
+
+    tj = round(j/(height-1),1)
+    ti = round(i/(width-1),1)
+
+    if(tj == 0.0): nj = 0
+    if(tj == 1.0): pj = 1
+    if(ti == 0.0): ni = 0
+    if(ti == 1.0): pi = 1
+
+def flip():
+    global turn,nj,pj,ni,pi
+
+    corner(y,x)
+
+    for b in range(nj,pj):
+        for a in range(ni,pi):
             try:
-                if(arr[x+a][y+b] == -1*value):
+                if(arr[y+b][x+a] == -1*turn):
 
                     index = 1
-                    while(arr[y+b*index][x+a*index] == -1*value):
+                    while(arr[y+b*index][x+a*index] == -1*turn):
                         cache.append(y+b*index)
                         cache.append(x+a*index)
                         index += 1
 
-                    if(arr[y+b*index][x+a*index] == value):
+                    if(arr[y+b*index][x+a*index] == turn):
                         for i in range(0,int(len(cache)/2)):
-                            arr[cache[i*2]][cache[i*2+1]] = value
+                            arr[cache[i*2]][cache[i*2+1]] = turn
                         cache = []
             except:
                 pass
 
-def check():
-    for y in range(0,height):
-        for x in range(0,width):
+def update():
+    global player,turn,nj,pj,ni,pi
 
-            for b in range(-1,2):
-                for a in range(-1,2):
-                    try:
-                        value = arr[y+b][x+a]
-                        if(value != 0):
+    for j in range(0,height):
+        for i in range(0,width):
+            if(arr[j][i] == 2):
+                arr[j][i] = 0
 
-                            if(value == -2 or value == 2):
-                                arr[y+b][x+a] = 0
+    flip()
+    
+    for j in range(0,height):
+        for i in range(0,width):
 
-                            elif(-2 < value < 2):
+            if(arr[j][i] == turn):
 
-                                index = 1
-                                while(arr[y+b*index][x+a*index] == -1*value):
-                                    index += 1
+                corner(j,i)
 
-                                if(arr[y+b*index][x+a*index] == 0):
-                                    arr[y+b*index][x+a*index] = value*2
-                    except:
-                        pass
+                for b in range(nj,pj):
+                    for a in range(ni,pi):
+
+                        if(arr[j+b][i+a] == -1*turn):
+                            
+                            index = 1
+                            while(j+index*b < height and i+index*a < width and arr[j+b*index][i+a*index] == -1*turn):
+                                index += 1
+                                
+                            if(j+index*b < height and i+index*a < width and arr[j+b*index][i+a*index] == 0):
+                                arr[j+b*index][i+a*index] = 2
 
 def write():
     window.fill(colors[0])
@@ -104,16 +132,16 @@ def write():
             value = arr[j][i]
             if(value != 0):
 
-                if(-2 < value < 2):
+                if(value == -1 or value == 1):
                     p.draw.circle(window,colors[value],(i*size+radi+1,j*size+radi+1),radi-5)
-                
-                else:
-                    if(value == -2):
-                        p.draw.circle(window,colors[2],(i*size+radi+1,j*size+radi+1),radi-2,2)
+
+                elif(value == 2 and player):
+                    p.draw.circle(window,colors[2],(i*size+radi+1,j*size+radi+1),radi-2,2)
 
 run = True
 while(run):
-    tick += 1
+
+    write()
 
     if(player == False):
         robot()
@@ -125,9 +153,6 @@ while(run):
 
         elif(event.type == p.MOUSEBUTTONDOWN and player):
             human()
-
-    check()
-    write()
 
     watch.tick(60)
     p.display.flip()
