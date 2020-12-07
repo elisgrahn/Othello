5d15c361c8d568c0259fe4775c3db56b9bb7bcc4
Elis Grahn <egge5173@gmail.com>
2020-12-07 21:22:39 +0100

    Refactoring

--- Othello.py
+++ Othello.py
@@ -8,7 +8,7 @@ height = 8
 size = 100
 tick = 0
 
-colors = [(0,0,0),(255,255,255),(0,144,103),(70,70,70)]
+colors = [(0,144,103),(255,255,255),(70,70,70),(0,0,0)]
 ry = random.randint(0,height-1)
 rx = random.randint(0,width-1)
 radi = int(size/2)
@@ -62,48 +62,54 @@ def flip(y,x,value):
                     if(arr[y+b*index][x+a*index] == value):
                         for i in range(0,int(len(cache)/2)):
                             arr[cache[i*2]][cache[i*2+1]] = value
-                    cache = []
+                        cache = []
             except:
                 pass
 
-def check(y,x,value):
-    for b in range(-1,2):
-        for a in range(-1,2):
-            try:
-                print("Ja")
-                if(arr[y+b][x+a] == -1*value):
+def check():
+    for y in range(0,height):
+        for x in range(0,width):
 
-                    index = 1
-                    while(arr[y+b*index][x+a*index] == -1*value):
-                        index += 1
+            for b in range(-1,2):
+                for a in range(-1,2):
+                    try:
+                        value = arr[y+b][x+a]
+                        if(value != 0):
 
-                    if(arr[y+b*index][x+a*index] == 0):
-                        arr[y+b*index][x+a*index] = value*2
-            except:
-                pass
+                            if(value == -2 or value == 2):
+                                arr[y+b][x+a] = 0
+
+                            elif(-2 < value < 2):
+
+                                index = 1
+                                while(arr[y+b*index][x+a*index] == -1*value):
+                                    index += 1
+
+                                if(arr[y+b*index][x+a*index] == 0):
+                                    arr[y+b*index][x+a*index] = value*2
+                    except:
+                        pass
 
 def write():
-    window.fill(colors[2])
+    window.fill(colors[0])
     for j in range(0,height):
 
         if(j <= width-1): 
-            p.draw.line(window,colors[0],(0,(j+1)*size),((width+1)*size,(j+1)*size),2)
+            p.draw.line(window,colors[3],(0,(j+1)*size),((width+1)*size,(j+1)*size),2)
 
         for i in range(0,width):
             if(j == 0 and i <= width-1): 
-                p.draw.line(window,colors[0],((i+1)*size,0),((i+1)*size,(height+1)*size),2)
+                p.draw.line(window,colors[3],((i+1)*size,0),((i+1)*size,(height+1)*size),2)
             
             value = arr[j][i]
             if(value != 0):
 
                 if(-2 < value < 2):
-                    p.draw.circle(window,colors[int((1+value)/2)],(i*size+radi+1,j*size+radi+1),radi-5)
-                    check(j,i,value)
+                    p.draw.circle(window,colors[value],(i*size+radi+1,j*size+radi+1),radi-5)
                 
                 else:
                     if(value == -2):
-                        p.draw.circle(window,colors[3],(i*size+radi+1,j*size+radi+1),radi-2,2)
-                    arr[j][i] = 0
+                        p.draw.circle(window,colors[2],(i*size+radi+1,j*size+radi+1),radi-2,2)
 
 run = True
 while(run):
@@ -120,11 +126,8 @@ while(run):
         elif(event.type == p.MOUSEBUTTONDOWN and player):
             human()
 
+    check()
     write()
-
-    if(tick%180 == 0):
-        print(numpy.array(arr))
-
 
     watch.tick(60)
     p.display.flip()
